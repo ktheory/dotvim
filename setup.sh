7e44f970d2759ef5362694549ed0abfa6c85583a
Aaron Suggs <aaron@ktheory.com>
2014-10-22 13:50:16 -0400

    modernize

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/vimrc.bundles ~/.vimrc.bundles
 ln -sf ~/.vim/gvimrc ~/.gvimrc
 
-vim -u ~/.vimrc.bundles +BundleInstall +qa
+if ! [ -e ~/.vim/bundle/Vundle.vim ];
+then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+set rtp+=~/.vim/bundle/Vundle.vim
