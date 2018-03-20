22412aa78da47c3028506bff1ade01f0a97d442d
Aaron Suggs <aaron@ktheory.com>
2018-03-20 13:32:13 -0400

    Update Vundle, remove unused plugins

--- setup.sh
+++ setup.sh
@@ -6,9 +6,11 @@ ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/vimrc.bundles ~/.vimrc.bundles
 ln -sf ~/.vim/gvimrc ~/.gvimrc
 
+mkdir -p ~/.vim/bundle
+
 if ! [ -e ~/.vim/bundle/Vundle.vim ];
 then
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 set rtp+=~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
